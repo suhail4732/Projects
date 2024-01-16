84c8727f853475ce912d1d03c8d3c1036f9cbe2a
Felix <felix.black01@gmail.com>
2024-01-16 12:17:53 +0000

    Effect of beam
    Created a field of pixels representing the height of the resist at each point. Reduce that height by DEI(r) where r is distance from the centre of the incoming beam
    
    common room

--- Watson's Method.py
+++ Watson's Method.py
@@ -1,2 +1,27 @@
 from matplotlib import pyplot as plt
 import numpy as np
+
+## Field of pixels to represent locations on sample
+pixels = np.ones((100, 100))    # lengths in nm
+
+beamLoc = [50, 50]
+
+
+def DEI(rsquared):
+    eta = 0.5
+    alpha = 5 # nm
+    beta = 5000 # nm
+    ## https://doi.org/10.1016/S0167-9317(00)00320-8, equation in introduction:
+    return 100/(1 + eta) * (((1/(np.pi * alpha**2)) * np.exp(-rsquared/alpha**2)) + ((eta/(np.pi * beta**2)) * np.exp(-rsquared/beta**2)))
+
+for x in range(len(pixels)):
+    for y in range(len(pixels[0])):
+        pixels[x][y] -= DEI((beamLoc[0] - x)**2 + (beamLoc[1] - y)**2)
+        pixels[x][y] = max(0, pixels[x][y])
+
+##         np.array([[1, 2, 3],
+##                   [4, 5, 6],
+##                   [7, 8, 9]])
+
+plt.matshow(pixels)
+plt.show()
