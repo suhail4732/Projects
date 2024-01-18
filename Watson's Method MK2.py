19c42a2d8e768ded52905188f504149e41624a8d
Felix <felix.black01@gmail.com>
2024-01-18 14:12:05 +0000

    Attempted to add iterative corrected dose
    common room

--- Watson's Method MK2.py
+++ Watson's Method MK2.py
@@ -14,10 +14,10 @@ sigma_A = 15   # * 50nm
 
 baseDosePower = 50
 
-shotPixels[int(  size/4)][int(size/8) : int(7*size/8) : int(size/20)] = baseDosePower
-shotPixels[int(3*size/4)][int(size/8) : int(7*size/8) : int(size/20)] = baseDosePower
+shotPixels[int(  size/4)][int(size/8) : int(7*size/8) : int(size/20)] = 1
+shotPixels[int(3*size/4)][int(size/8) : int(7*size/8) : int(size/20)] = 1
 for x in range(int(size/4), int(3*size/4), int(size/20)):
-    shotPixels[int(x)][int(  size/8)] = baseDosePower
+    shotPixels[int(x)][int(  size/8)] = 1
 
 plt.matshow(shotPixels)
 plt.show()
@@ -30,27 +30,33 @@ for x in range(size):
     for y in range(size):
         scatterDistr[x][y] += (P_B(dist := (x-size/2)**2 + (y-size/2)**2) + P_A(dist))
 
-plt.matshow(scatterDistr)
-plt.show()
-
 ##plt.matshow(scatterDistr)
 ##plt.show()
 
 
 ##for doseCount in range(1):
-print("Convolution:")
-dosePixels = scipy.signal.convolve2d(shotPixels, scatterDistr, mode = "same", boundary = "fill", fillvalue = "0")
+print("First convolution: ", end = "")
+dosePixels = baseDosePower * scipy.signal.convolve2d(shotPixels, scatterDistr, mode = "same", boundary = "fill", fillvalue = "0")
 print("done")
 ##    dosePixels[0][0] = 1
 ##    dosePixels[-1][-1] = 0
 
-##plt.figure()
-##ax = fig.add_subplot(111)
 plt.matshow(dosePixels)
 plt.colorbar()
-
 plt.show()
 
-samplePixels = np.where(dosePixels > 1, 1, 0)
+correctedShots = 2 * shotPixels * (2 - dosePixels)
+plt.matshow(correctedShots)
+plt.show()
+
+print("Corrected convolution: ", end = "")
+correctedDosePixels = baseDosePower * scipy.signal.convolve2d(correctedShots, scatterDistr, mode = "same", boundary = "fill", fillvalue = "0")
+print("done")
+
+plt.matshow(correctedDosePixels)
+plt.colorbar()
+plt.show()
+
+samplePixels = np.where(correctedDosePixels > 1, 1, 0)
 plt.matshow(samplePixels)
 plt.show()
